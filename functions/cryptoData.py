Fixed the different functions. Added run time calculation.

functions/cryptoData.py
@@ -8,11 +8,11 @@ FILE_NAME = 'BITSTAMPEUR.csv'
 
 
 def load_data():
-    orig_btc_df = pd.read_csv(f"../data/{FILE_NAME}")
+    orig_btc_df = pd.read_csv(f"./data/{FILE_NAME}")
     # Clearing the columns names
     orig_btc_df.columns = ['time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Volume_CUR', 'Weighted_Price']
     # Set index for the Dataframe. Index is Time.
-    orig_btc_df.set_index('time', inplace=True)
+    orig_btc_df.set_index("time", inplace=True)
     # Executing functions, which modifies the data for the preprocess of algorithm.
     btc_df = modify_data(orig_btc_df)
 
@@ -21,16 +21,17 @@ def load_data():
 
 
 def modify_data(original_df):
-    new_df = original_df
-    # Adding two news columns: 'Close_Off_High' - daily average; 'Votalitily' - The difference when compared to previous date.
-    kwargs = {'Close_Off_High': lambda x: 2 * (x['High'] - x['Close']) / (x['High'] - x['Low']) - 1,
-              'Volatility': lambda x: (x['High'] - x['Low'] / x['Open'])}
+    # new_df = original_df
+    # Adding two news columns: 'Close_Off_High' - daily average;
+    # 'Votalitily' - The difference when compared to previous date.
+    kwargs = {'close_off_high': lambda x: 2 * (x['High'] - x['Close']) / (x['High'] - x['Low']) - 1,
+              'volatility': lambda x: (x['High'] - x['Low'] / x['Open'])}
     # Assign the two new columns to the main dataset
-    new_df = new_df.assign(**kwargs)
+    new_df = original_df.assign(**kwargs)
     # Removing the rows with values - 'NaN' or 'inf'.
     new_df.dropna(inplace=True)
     # Leaving only the important columns
-    new_df = new_df[['Close', 'Volume', 'Close_Off_High', 'Volatility']]
+    new_df = new_df[['Close', 'Volume', 'close_off_high', 'volatility']]
 
     print('The data has been modified and prepare for program to process it')
     return new_df
@@ -38,7 +39,6 @@ def modify_data(original_df):
 
 def normalize_data(data_set, window_size, norm_col):
     data_inputs = []
-    data_inputs_2 = pd.DataFrame()
     # For each row in the data set.
     # The normalizing must be done. Which means that the value is between -1 and 1, when comparing
     # to first value of window
@@ -48,14 +48,6 @@ def normalize_data(data_set, window_size, norm_col):
             temp_set.loc[:, col] = temp_set[col] / temp_set[col].iloc[0] - 1
         data_inputs.append(temp_set)
 
-    print(data_inputs)
-    for col in norm_col:
-        data_set[col] = data_set[col].pct_change()
-        data_set.dropna(inplace=True)
-        data_inputs_2[col] = preprocessing.scale(data_set[col].values)
-
-    print(data_inputs_2)
-
     data_inputs = [np.array(data_input) for data_input in data_inputs]
     data_inputs = np.array(data_inputs)
 
@@ -63,30 +55,18 @@ def normalize_data(data_set, window_size, norm_col):
     return data_inputs
 
 
-# Training output calculation
-def data_output(data_training, data_test, pred_range, window_size):
-    training_output = []
-    for i in range(window_size, len(data_training['Close']) - pred_range):
-        training_output.append((data_training['Close'][i:i+pred_range].values /
-                                data_training['Close'].values[i-window_size]) -1)
-    test_output = (data_test['Close'][window_size:].values / data_test['Close'][:-window_size].values) - 1
 
-    print('The calculation of training and test Outputs are finished')
-    return training_output, test_output
 
 
 # Function to save the pickle dump and check if it's there.
-def pickle_data(file_name, data):
-    # data_inputs = []
-    if os.path.isfile(f'./pickles/{file_name}.pkl'):
-        with open(f'{file_name}.pkl', 'rb') as f:
-            data_inputs = pickle.load(f)
-        print(f'Data has been get from file {file_name}.pkl')
-        return data_inputs
-    else:
-        with open(f'{file_name}.pkl', 'wb') as f:
-            pickle.dump(data, f)
-        print(f'Data has been dumped into {file_name}.pkl file')
-
-
-print(load_data())
+# def pickle_data(file_name, data):
+#     # data_inputs = []
+#     if os.path.isfile(f'./pickles/{file_name}.pkl'):
+#         with open(f'{file_name}.pkl', 'rb') as f:
+#             data_inputs = pickle.load(f)
+#         print(f'Data has been get from file {file_name}.pkl')
+#         return data_inputs
+#     else:
+#         with open(f'{file_name}.pkl', 'wb') as f:
+#             pickle.dump(data, f)
+#         print(f'Data has been dumped into {file_name}.pkl file')

functions/lstm.py
@@ -4,12 +4,16 @@ from keras.layers import LSTM, CuDNNLSTM, BatchNormalization
 from keras.callbacks import TensorBoard
 
 import numpy as np
+import time
 
-ACTIV_FUNC = 'linear'
+ACTIV_FUNC = "linear"
 LOSS = 'mae'
 OPTIMIZER = 'adam'
 DROPOUT = 0.25
 NEURONS = 128
+
+NAME = f'BTC_model-{int(time.time())}'
+LOG_NAME = f'Model-logs{int(time.time())}'
 
 
 def build_model(inputs, output_size):
@@ -18,26 +22,34 @@ def build_model(inputs, output_size):
     model.add(LSTM(NEURONS, input_shape=(inputs.shape[1], inputs.shape[2])))
     model.add(Dropout(DROPOUT))
     model.add(Dense(units=output_size))
-    model.add(ACTIV_FUNC)
+    model.add(Activation(ACTIV_FUNC))
 
     model.compile(loss=LOSS, optimizer=OPTIMIZER, metrics=['mae', 'acc'])
+
+    print(f'Model finished compiling and it is saved as {NAME}')
+    model.save(f'models/{NAME}')
     return model
 
 
-def fit_model(model, data_inputs, data_output, pred_range, tensorboard):
+def predict_model(model, data_inputs, data_output, pred_range,
+                  test_input, data_test, window_size):
+    tensorboard = TensorBoard(log_dir=f"logs/{LOG_NAME}")
+
     model_calc = model.fit(data_inputs[:-pred_range], data_output,
                            epochs=50,
                            batch_size=1,
                            verbose=2,
                            shuffle=True,
                            callbacks=[tensorboard])
+    print(f'Model have been trained and saved as tensorboard logs - {LOG_NAME}')
 
-    return model_calc
-
-
-def predict_prices(model, test_input, pred_range, data_test, window_size):
     btc_pred = ((model.predict(data_test)[:-pred_range][::pred_range] + 1) *
                 test_input['Close'].values[:-(window_size + pred_range)][::5].reshape(
-                    int(np.ceil((len(test_input) - pred_range) / float(pred_range))), 1))
-    # pickle function need to add...
+                    int(np.ceil((len(data_test) - pred_range) / float(pred_range))), 1))
+    print('BTC prediction prices calculated.')
     return btc_pred
+
+# def predict_prices(model, test_input, pred_range, data_test, window_size):
+#
+#     # pickle function need to add...
+#     return btc_pred

functions/preprocess.py
@@ -1,37 +1,39 @@
 import pandas as pd
 import pickle
 import os
+import numpy as np
 from functions import cryptoData
 
-DIR_BACKUP = './backup/'
-DIR_DATA = './data/'
-DIR_FUNCTIONS = './functions'
 
-SPLIT_TIME = '2019-01-28'
-WINDOW_SIZE = 10
-NORM_COLS = ['Close', 'Volume']
-
-PRED_RANGE = 5
-
-
-def preprocess_df(btc_df):
+def preprocess_df(btc_df, split_time, window_size, norm_cols):
     # Sorting the main dataframe
     btc_df.sort_values(by='time')
 
     # Splitting the data to two datasets - the training set will have 90% of data, and test set the rest.
-    training_set = btc_df[btc_df.index < SPLIT_TIME]
-    test_set = btc_df[btc_df.index >= SPLIT_TIME]
+    training_set = btc_df[btc_df.index < split_time]
+    test_set = btc_df[btc_df.index >= split_time]
     print('Training and test sets has been created')
-    lstm_training_inputs = cryptoData.normalize_data(training_set, WINDOW_SIZE, NORM_COLS)
-    lstm_test_inputs = cryptoData.normalize_data(test_set, WINDOW_SIZE, NORM_COLS)
+    lstm_training_inputs = cryptoData.normalize_data(training_set, window_size, norm_cols)
+    lstm_test_inputs = cryptoData.normalize_data(test_set, window_size, norm_cols)
 
     # pickle saving...
     # if
 
-    print('The data has been modified, normalized and temporary saved')
+    # print('The data has been modified, normalized and temporary saved')
     print('Now the algorithm building begins...')
+    return lstm_training_inputs, lstm_test_inputs, training_set, test_set
 
 
+# Training output calculation
+def data_output(data_training, data_test, window_size, pred_range):
+    training_array = []
+    for i in range(window_size, len(data_training['Close']) - pred_range):
+        training_array.append((data_training['Close'][i:i + pred_range].values /
+                                data_training['Close'].values[i - window_size]) - 1)
 
+    test_output = (data_test['Close'][window_size:].values /
+                   data_test['Close'][:-window_size].values) - 1
+    training_output = np.array(training_array)
 
-
+    print('The calculation of training and test Outputs are finished')
+    return training_output, test_output