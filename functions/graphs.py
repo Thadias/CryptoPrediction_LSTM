43bdfe6a5653abb05c9187ccfa67ed0181dfdd39
Tadiaush <tadas.bobinas@gmail.com>
2019-06-02 20:13:43 +0300

    Graphs now has proper ticks. Comparison algorithms are added.

--- functions/graphs.py
+++ functions/graphs.py
@@ -7,8 +7,6 @@ def bitcoin_price_graph(df):
     ax1 = plt.axes()
     ax1.set_ylabel('Closing Price ($)', fontsize=12)
     ax1.set_xlabel('Time (MM/YY)', fontsize=12)
-    # ax1.set_xticks([datetime.time(i, j, 1).strftime('%b %d %Y') for i in range(2018, 2019)
-    #                 for j in [1, 12]])
     ax1.plot(df.index, df['Close'])
     plt.show()
 
@@ -16,8 +14,8 @@ def bitcoin_price_graph(df):
 # def bitcoin_pred_graph(main_df, test_input, model, split_date, window_size):
 def bitcoin_pred_graph(main_df, pred_prices, split_date, window_size, pred_range):
     temp_df = main_df[main_df.index >= split_date]
-    temp_x1 = temp_df.index[window_size + 3:].astype(datetime.date)
-    temp_x2 = temp_df.index[window_size + 3:].astype(datetime.date)  # [pred_range: pred_range + pred_range]
+    temp_x1 = temp_df.index[window_size + 3:].astype(datetime.datetime)
+    temp_x2 = temp_df.index[window_size + 3:].astype(datetime.datetime)  # [pred_range: pred_range + pred_range]
     temp_y1 = temp_df['Close'][window_size + 3:]
     temp_y2 = pred_prices['Pred']
 
@@ -27,8 +25,9 @@ def bitcoin_pred_graph(main_df, pred_prices, split_date, window_size, pred_range
     plt.grid(True)
     plt.plot(temp_x1, temp_y1, label='Actual')
     plt.plot(temp_x2, temp_y2, label='Predicted')
+    plt.xticks(rotation=90)
     plt.ylabel('Bitcoin Price ($)', fontsize=12)
-    plt.xlabel('Time (MM/YY)', fontsize=12)
+    plt.xlabel('Time (YY/MM/DD)', fontsize=12)
     plt.legend(bbox_to_anchor=(0.1, 1), loc=2, borderaxespad=0., prop={'size': 12})
     plt.show()
 
